22fad89f25bf655550ffcb8fa9f676934285da59
parthghori <88545341+ParthGhori@users.noreply.github.com>
2023-07-02 17:03:07 +0530

    Add files via upload

---
+++ src/components/Clients.jsx
@@ -0,0 +1,27 @@
+import { clients } from "../constants";
+import styles from "../style";
+
+const Clients = () => {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div
+        className={`${styles.flexCenter} flex-wrap w-[900px] hover:t ext-white`}
+      >
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] hover:bg-dimWhite cursor-pointer`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="sm:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Clients;
